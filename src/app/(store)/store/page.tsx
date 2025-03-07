b36b8ab0f28a9d3893ea9c1fa2cc08ad321e840c
rodrigorvsn <rodrigovictor81@gmail.com>
2025-03-07 06:58:53 -0300

    feat: create store page

--- src/app/(store)/store/page.tsx
+++ src/app/(store)/store/page.tsx
@@ -1,7 +1,42 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { ChevronLeftIcon } from "lucide-react";
+import { bookstore } from "@/../public/bookstore.json";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 export default function Store() {
-  return <section>hi</section>;
+  const books = bookstore.genres.flatMap((genre) => {
+    return genre.books.map((book) => ({ ...book, genre: genre.name }));
+  });
+
+  return (
+    <section className="m-4 md:m-16">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Author</TableHead>
+            <TableHead>Genre</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>ISBN</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {books.map((book) => (
+            <TableRow key={book.isbn}>
+              <TableCell>{book.name}</TableCell>
+              <TableCell>{book.author}</TableCell>
+              <TableCell>{book.genre}</TableCell>
+              <TableCell>{book.description}</TableCell>
+              <TableCell>{book.isbn}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  );
 }
